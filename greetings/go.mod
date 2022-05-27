429d605251e13ded43223a71da8135ac11807fc9
nixil <nixil.itp@gmail.com>
2022-05-27 14:48:06 -0400

    add greetings.go file

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module github.com/yuelinxing/gateio/greetings
+
+go 1.18
---
+++ greetings/greetings.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	
+)
+
+func main(){
+     message := "greetings"
+     fmt.Println(message)
+}
